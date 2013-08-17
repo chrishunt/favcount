164898c7fabf9fdfe365f62d59e13b979b9e3f8d
Chris Hunt <chrishunt@github.com>
2013-08-17 16:03:05 -0700

    Add drawCanvas() for drawing number on icon canvas

--- favcount.js
+++ favcount.js
@@ -16,48 +16,50 @@
   Favcount.VERSION = '1.0.0';
 
   Favcount.prototype.set = function(count) {
-    var favcount = this,
-        img = document.createElement('img'),
-        link = document.createElement('link'),
-        multiplier, fontSize, context, xOffset, yOffset;
+    var self = this,
+        img = document.createElement('img');
 
-    if (favcount.canvas.getContext) {
+    if (self.canvas.getContext) {
            if (count < 1)  { count = '' }
       else if (count < 10) { count = ' ' + count }
       else if (count > 99) { count = '99' }
 
-      img.onload = function() {
-        favcount.canvas.height = favcount.canvas.width = this.width;
-        multiplier = (this.width / 16);
-
-        fontSize = multiplier * 11;
-        xOffset  = multiplier;
-        yOffset  = multiplier * 11;
-
-        context = favcount.canvas.getContext('2d');
-        context.drawImage(this, 0, 0);
-        context.font = 'bold ' + fontSize + 'px "helvetica", sans-serif';
-
-        context.fillStyle = '#FFF';
-        context.fillText(count, xOffset, yOffset);
-        context.fillText(count, xOffset + 2, yOffset);
-        context.fillText(count, xOffset, yOffset + 2);
-        context.fillText(count, xOffset + 2, yOffset + 2);
-
-        context.fillStyle = '#000';
-        context.fillText(count, xOffset + 1, yOffset + 1);
-
-        document.getElementsByTagName('head')[0].removeChild(
-          document.querySelector('link[rel$=icon]')
-        );
-
-        link.rel = 'shortcut icon';
-        link.href = favcount.canvas.toDataURL('image/png');
-        document.getElementsByTagName('head')[0].appendChild(link);
-      };
-
+      img.onload = function() { drawCanvas(self.canvas, img, count); };
       img.src = this.icon;
     }
+  }
+
+  function drawCanvas(canvas, img, count) {
+    var head = document.getElementsByTagName('head')[0],
+        favicon = document.createElement('link'),
+        multiplier, fontSize, context, xOffset, yOffset;
+
+    favicon.rel = 'shortcut icon';
+
+    multiplier = (img.width / 16);
+    fontSize   = multiplier * 11;
+    xOffset    = multiplier;
+    yOffset    = multiplier * 11;
+
+    canvas.height = canvas.width = img.width;
+
+    context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    context.font = 'bold ' + fontSize + 'px "helvetica", sans-serif';
+
+    context.fillStyle = '#FFF';
+    context.fillText(count, xOffset, yOffset);
+    context.fillText(count, xOffset + 2, yOffset);
+    context.fillText(count, xOffset, yOffset + 2);
+    context.fillText(count, xOffset + 2, yOffset + 2);
+
+    context.fillStyle = '#000';
+    context.fillText(count, xOffset + 1, yOffset + 1);
+
+    favicon.href = canvas.toDataURL('image/png');
+
+    head.removeChild(document.querySelector('link[rel$=icon]'));
+    head.appendChild(favicon);
   }
 
   this.Favcount = Favcount;
