a2cd3126ebe7f55047d980ae71e296ea63bfca8a
Chris Hunt <chrishunt@github.com>
2013-08-17 23:54:53 -0700

    Change 'icon' to 'favicon' in examples

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,9 @@
-var icon = new Favcount('icons/blue-dot.ico');
+var favicon = new Favcount('icons/blue-dot.ico');
 
 function setCount(count) {
   if (count > 99) { count = 1 };
 
-  icon.set(count);
+  favicon.set(count);
 
   setTimeout(function() {
     setCount(count + 1);
