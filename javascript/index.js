e3a3ce8316ce3bf7b86af56c4ac0cab31b40a743
Chris Hunt <chrishunt@github.com>
2013-08-18 00:11:10 -0700

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+var favicon = new Favcount('icons/blue-dot.ico');
+
+function setCount(count) {
+  if (count > 99) { count = 1 };
+
+  favicon.set(count);
+
+  setTimeout(function() {
+    setCount(count + 1);
+  }, 500);
+}
+
+setCount(1);
