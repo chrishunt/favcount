d47d2aa36ae0932b6bc98c4423399d95a96060f9
russenreaktor <m.kupriyanov@gmail.com>
2013-09-05 00:01:52 +0200

    refactored favcount added method "render"

--- favcount.js
+++ favcount.js
@@ -7,93 +7,107 @@
  * Released under the MIT license
  */
 
-(function(){
-  function Favcount(icon) {
-    this.icon = icon;
-    this.opacity = 0.4;
-    this.canvas = document.createElement('canvas');
-  }
 
-  Favcount.prototype.set = function(count) {
-    var self = this,
+'use strict';
+(function () {
+    function Favcount(icon) {
+        this.icon = icon;
+        this.opacity = 0.4;
+        this.canvas = document.createElement('canvas');
+    }
+
+    Favcount.prototype.set = function(count) {
+        var self = this,
+        head = document.getElementsByTagName('head')[0],
+        favicon = document.createElement('link');
+
+        self.render(count, function(dataUrl){
+            favicon.rel = 'icon';
+            favicon.href = dataUrl;
+            var node = document.querySelector('link[rel=icon]');
+            if (node) {
+                head.removeChild();
+            }
+            head.appendChild(favicon);
+        });
+
+    };
+
+    Favcount.prototype.render = function(count, callback) {
+        var self = this,
         img  = document.createElement('img');
 
-    if (self.canvas.getContext) {
-      img.onload = function() {
-        drawCanvas(self.canvas, self.opacity, img, normalize(count));
-      };
+        if (self.canvas.getContext) {
+            img.onload = function() {
+                if (typeof callback === 'function') {
+                    callback(drawCanvas(self.canvas, self.opacity, img, normalize(count)));
+                }
+            };
 
-      img.src = this.icon;
+            img.src = this.icon;
+        }
+    };
+
+    function normalize(count) {
+        count = Math.round(count);
+
+        if (isNaN(count) || count < 1) {
+            return '';
+        } else if (count < 10) {
+            return ' ' + count;
+        } else if (count > 99) {
+            return '99';
+        } else {
+            return count;
+        }
     }
-  }
 
-  function normalize(count) {
-    count = Math.round(count);
+    function drawCanvas(canvas, opacity, img, count) {
+        var multiplier, fontSize, context, xOffset, yOffset, border, shadow;
 
-    if (isNaN(count) || count < 1) {
-      return '';
-    } else if (count < 10) {
-      return ' ' + count;
-    } else if (count > 99) {
-      return '99';
-    } else {
-      return count;
+        // Scale canvas elements based on favicon size
+        multiplier = img.width / 16;
+        fontSize   = multiplier * 11;
+        xOffset    = multiplier;
+        yOffset    = multiplier * 11;
+        border     = multiplier;
+        shadow     = multiplier * 2;
+
+        canvas.height = canvas.width = img.width;
+        context = canvas.getContext('2d');
+        context.font = 'bold ' + fontSize + 'px "helvetica", sans-serif';
+
+        // Draw faded favicon background
+        if (count) { context.globalAlpha = opacity; }
+        context.drawImage(img, 0, 0);
+        context.globalAlpha = 1.0;
+
+        // Draw white drop shadow
+        context.shadowColor = '#FFF';
+        context.shadowBlur = shadow;
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+
+        // Draw white border
+        context.fillStyle = '#FFF';
+        context.fillText(count, xOffset, yOffset);
+        context.fillText(count, xOffset + border, yOffset);
+        context.fillText(count, xOffset, yOffset + border);
+        context.fillText(count, xOffset + border, yOffset + border);
+
+        // Draw black count
+        context.fillStyle = '#000';
+        context.fillText(count,
+          xOffset + (border / 2.0),
+          yOffset + (border / 2.0)
+        );
+
+        return canvas.toDataURL('image/png');
     }
-  }
 
-  function drawCanvas(canvas, opacity, img, count) {
-    var head = document.getElementsByTagName('head')[0],
-        favicon = document.createElement('link'),
-        multiplier, fontSize, context, xOffset, yOffset, border, shadow;
-
-    favicon.rel = 'icon';
-
-    // Scale canvas elements based on favicon size
-    multiplier = img.width / 16;
-    fontSize   = multiplier * 11;
-    xOffset    = multiplier;
-    yOffset    = multiplier * 11;
-    border     = multiplier;
-    shadow     = multiplier * 2;
-
-    canvas.height = canvas.width = img.width;
-    context = canvas.getContext('2d');
-    context.font = 'bold ' + fontSize + 'px "helvetica", sans-serif';
-
-    // Draw faded favicon background
-    if (count) { context.globalAlpha = opacity; }
-    context.drawImage(img, 0, 0);
-    context.globalAlpha = 1.0;
-
-    // Draw white drop shadow
-    context.shadowColor = '#FFF';
-    context.shadowBlur = shadow;
-    context.shadowOffsetX = 0;
-    context.shadowOffsetY = 0;
-
-    // Draw white border
-    context.fillStyle = '#FFF';
-    context.fillText(count, xOffset, yOffset);
-    context.fillText(count, xOffset + border, yOffset);
-    context.fillText(count, xOffset, yOffset + border);
-    context.fillText(count, xOffset + border, yOffset + border);
-
-    // Draw black count
-    context.fillStyle = '#000';
-    context.fillText(count,
-      xOffset + (border / 2.0),
-      yOffset + (border / 2.0)
-    );
-
-    // Replace favicon with new favicon
-    favicon.href = canvas.toDataURL('image/png');
-    head.removeChild(document.querySelector('link[rel=icon]'));
-    head.appendChild(favicon);
-  }
-
-  this.Favcount = Favcount;
+    this.Favcount = Favcount;
 }).call(this);
 
 (function(){
-  Favcount.VERSION = '1.2.0';
+    Favcount.VERSION = '1.2.0';
 }).call(this);
